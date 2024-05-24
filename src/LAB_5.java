0760380b7626d05391cca7943549d24f1fb77d8b
vital999 <roma.ne.roma28@gmail.com>
2024-05-24 13:15:18 +0300

    upd

--- src/LAB_5.java
+++ src/LAB_5.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class LAB_5 {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        // Print the area of a rectangle with width 5 and height 8
+        System.out.println("Площа прямокутника: " + getRectangleArea(5, 8));
+
+        // Print the smallest of -5, 12, and 2
+        System.out.printf("Найменше число: %s%n", findMin( 5, 12, 2));
+
+        // Print an array
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        printArray(arr);
+
+        // Print the largest number in an array
+        System.out.println("Найбільше число в масиві таке: " + findMax(arr));
+    }
+
+
+    public static double getRectangleArea(double width, double height) {
+        return width * height;
+    }
+
+
+    public static String findMin(int num1, int num2, int num3) {
+        int min = Math.min(num1, Math.min(num2, num3));
+        System.out.println(min);
+        return null;
+    }
+
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
         }
+        System.out.println();
+    }
+
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int value : arr) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
     }
 }
